1335f111ecc004a48641089d2c2ab3d05940bb26
LouisCapo <rusinov290701@gmail.com>
2021-01-30 15:34:31 +0300

    [+] Добавил team сервис

--- src/app/modules/team-page/components/team-info/team-info.component.ts
+++ src/app/modules/team-page/components/team-info/team-info.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ITeam } from '../../model/team-interfaces'
+import { TeamService } from '../../services/team.service'
 
 @Component({
   selector: 'app-team-info',
   templateUrl: './team-info.component.html',
   styleUrls: ['./team-info.component.scss']
 })
-export class TeamInfoComponent implements OnInit {
+export class TeamInfoComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input() teamId: string;
+
+  teamInfo: ITeam;
+
+  private _subscription: Subscription[] = [];
+
+  constructor(private _teamService: TeamService) { }
 
   ngOnInit() {
+    this._subscription.push(this._teamService.getTeam(this.teamId).subscribe(res => {
+      this.teamInfo = res;
+    }))
+  }
+
+  ngOnDestroy() {
+    this._subscription.forEach(res => {
+      res.unsubscribe();
+    })
   }
 
 }
---
+++ src/app/modules/team-page/model/team-interfaces.ts
@@ -0,0 +1,32 @@
+export interface ITeam {
+	teamId: string;
+	teamName: string;
+	teamLogo: string;
+	teamMembers: ITeamMember[];
+	teamStats: ITeamStats[];
+	lastMatches: ILastMatch[];
+}
+
+interface ITeamMember {
+	memberNick: string;
+	memberPhoto: string;
+	memberId: string;
+}
+
+interface ITeamStats {
+	title: string;
+	value: string;
+}
+
+interface ILastMatch {
+	firstTeam: ITeamInfo;
+	secondTeam: ITeamInfo;
+	winningTeam: string;
+	matchScore: string;
+}
+
+interface ITeamInfo {
+	teamName: string;
+	teamLogo: string;
+	teamId: string;
+}
